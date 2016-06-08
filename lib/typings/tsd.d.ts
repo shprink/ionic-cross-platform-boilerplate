adc7f781dab580d33aaef192678208e7265525db
Rich Christiansen <rich@byu.net>
2016-06-08 13:06:11 -0600

    Migrating from TSD to typings
    TSD has been deprecated
    (https://github.com/DefinitelyTyped/tsd/issues/269), with typings
    suggested as the replacement.  These changes migrate the code from TSD
    to typings.

--- lib/typings/tsd.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="./ionic/ionic.d.ts" />
-/// <reference path="./angular-ui-router/angular-ui-router.d.ts" />
-/// <reference path="./require.d.ts" />
-/// <reference path="./cordova/cordova.d.ts" />
-/// <reference path="./ng-cordova/tsd.d.ts" />
-/// <reference path="./webpack.d.ts" />
